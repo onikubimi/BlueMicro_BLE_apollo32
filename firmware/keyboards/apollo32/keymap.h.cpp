fb12ccaeeb2b113510b1ccd6111a3cdc8f6774f5
hatoP <steeeevor@yahoo.co.jp>
2025-05-30 13:07:01 +0900

    Update keymap.h.cpp

--- firmware/keyboards/apollo32/keymap.h.cpp
+++ firmware/keyboards/apollo32/keymap.h.cpp
@@ -1,11 +1,18 @@
 #include "keymap.h"
+#include <array>
 
-std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-    KEYMAP2ARRAY(KEYMAP(
-        KC_Q,    KC_W,    
-        KC_A,    KC_S 
-        ));
+std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix = {
+    KEYMAP(
+        KC_Q, KC_W, /* 必要に応じてキーを追加 */
+        KC_A, KC_S  /* 必要に応じてキーを追加 */
+        /* ... 他のキー ... */
+    )
+};
 
 void setupKeymap() {
-  ;  // this function does nothing in this case.
+    for (int row = 0; row < MATRIX_ROWS; ++row) {
+        for (int col = 0; col < MATRIX_COLS; ++col) {
+            matrix[row][col].addActivation(_L0, Method::PRESS, matrix[row][col].getKeycode());
+        }
+    }
 }
