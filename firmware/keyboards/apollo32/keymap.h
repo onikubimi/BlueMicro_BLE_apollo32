14320817b826a903363f3c9cd20d358199715d8e
hatoP <steeeevor@yahoo.co.jp>
2025-05-30 12:57:21 +0900

    Update keymap.h

--- firmware/keyboards/apollo32/keymap.h
+++ firmware/keyboards/apollo32/keymap.h
@@ -1,2 +1,10 @@
+#include <stdint.h>
+#include "hid_keycodes.h"
+#include "hardware_variants.h"
+#include "keyboard_config.h"
+#include "advanced_keycodes.h"
+#include "Key.h"
+#include <array>
+
 void setupKeymap();
 extern std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix; 
